d087d9d905be4af9fe5ecd1b086e75b14227ce7b
Kavindu Jayakodi <kavindujd1995@gmail.com>
2025-01-01 04:04:28 +0530

    test: test http get request

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('./dbconection/mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.post('/tasks', async (req, res) => {
+app.get('/tasks', (req, res) => {
     res.send("testing");
 })
 
