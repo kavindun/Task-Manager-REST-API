92198825c749a111e901eede10179886d428656e
Kavindu Jayakodi <kavindujd1995@gmail.com>
2025-01-01 16:00:27 +0530

    feat: add mongodb connection

--- src/dbconection/mongoose.js
+++ src/dbconection/mongoose.js
@@ -3,5 +3,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/Local-Database-Mongodb', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
+
+module.exports  = mongoose
