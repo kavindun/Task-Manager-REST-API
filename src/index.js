771866eb4d8791eb289a981191b6427e43b0ee4b
Kavindu Jayakodi <kavindujd1995@gmail.com>
2025-01-02 04:25:38 +0530

    feat: add user route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./dbconection/mongoose')
 const taskRouter = require('./routes/task')
+const userRouter = require('./routes/user')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(taskRouter)
+app.use(userRouter)
 
 
 
